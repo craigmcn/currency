d2a16c0d567fe5ddf0b8606a9afba59f21b00556
Craig McNaughton <therealcraigmcn@gmail.com>
2022-01-03 12:53:54 -0800

    Patch to version 2.2.9
    - npm update, audit fix
    - update devServer to port 3030
    - add .nvmrc file

--- webpack.config.js
+++ webpack.config.js
@@ -1,117 +1,118 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 const config = {
-  entry: {
-    app: './src/index.tsx',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 4000,
-    static: {
-        directory: './tmp',
-        publicPath: '/',
-    }
-  },
-  output: {
-    filename: '[name].[contenthash].bundle.js',
-    publicPath: './',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new LodashModuleReplacementPlugin(),
-    new Dotenv({ systemvars: true }),
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: [/node_modules/, /temp/],
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.ts(x?)$/,
-        exclude: [/node_modules/, /temp/],
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-  optimization: {
-    usedExports: true,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          // cacheGroupKey here is `commons` as the key of the cacheGroup
-          name(module, chunks, cacheGroupKey) {
-            const moduleFileName = module
-              .identifier()
-              .replace(/\\/g, '/')
-              .split('/')
-              .reduceRight((item) => item);
-            const allChunksNames = chunks.map((item) => item.name).join('~');
-            return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
-          },
-          chunks: 'all',
-        },
-        defaultVendors: {
-          enforce: true,
-          reuseExistingChunk: true,
-        },
-      },
+    entry: {
+        app: './src/index.tsx',
     },
-  },
-  node: {
-    global: false,
-    __filename: false,
-    __dirname: false,
-  },
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 3030,
+        static: {
+            directory: './tmp',
+            publicPath: '/',
+        }
+    },
+    output: {
+        filename: '[name].[contenthash].bundle.js',
+        publicPath: './',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new LodashModuleReplacementPlugin(),
+        new Dotenv({
+            systemvars: true
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/, /temp/],
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.ts(x?)$/,
+                exclude: [/node_modules/, /temp/],
+                use: [{
+                    loader: 'ts-loader',
+                }, ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    optimization: {
+        usedExports: true,
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    // cacheGroupKey here is `commons` as the key of the cacheGroup
+                    name(module, chunks, cacheGroupKey) {
+                        const moduleFileName = module
+                            .identifier()
+                            .replace(/\\/g, '/')
+                            .split('/')
+                            .reduceRight((item) => item);
+                        const allChunksNames = chunks.map((item) => item.name).join('~');
+                        return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+                    },
+                    chunks: 'all',
+                },
+                defaultVendors: {
+                    enforce: true,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
+    },
+    node: {
+        global: false,
+        __filename: false,
+        __dirname: false,
+    },
 };
 
 module.exports = (env) => {
-  const distConfig = Object.assign({}, config, {
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-    },
-  });
+    const distConfig = Object.assign({}, config, {
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+        },
+    });
 
-  const netlifyRootConfig = Object.assign({}, config, {
-    output: {
-      path: path.resolve(__dirname, 'netlify'),
-    },
-  });
+    const netlifyRootConfig = Object.assign({}, config, {
+        output: {
+            path: path.resolve(__dirname, 'netlify'),
+        },
+    });
 
-  const netlifySubConfig = Object.assign({}, config, {
-    output: {
-      path: path.resolve(__dirname, 'netlify', 'currency'),
-    },
-  });
+    const netlifySubConfig = Object.assign({}, config, {
+        output: {
+            path: path.resolve(__dirname, 'netlify', 'currency'),
+        },
+    });
 
-  return env.netlify ? [netlifyRootConfig, netlifySubConfig] : distConfig;
+    return env.netlify ? [netlifyRootConfig, netlifySubConfig] : distConfig;
 };
